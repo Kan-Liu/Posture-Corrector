9bf55a0a1222a3bec4fc9f6e36fb1714ad832e27
Jackson Gayda <t-jagayd@microsoft.com>
2020-07-29 15:33:11 -0700

    Format document and hide time input if notifications are off

--- posture-corrector/src/components/Notifications.js
+++ posture-corrector/src/components/Notifications.js
@@ -2,9 +2,9 @@ import React, { createRef } from "react";
 import ReactNotification from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
 import { store } from "react-notifications-component";
-import { PrimaryButton, Stack } from 'office-ui-fabric-react';
-import { TextField } from 'office-ui-fabric-react/lib/TextField';
-import { Toggle } from 'office-ui-fabric-react/lib/Toggle';
+import { PrimaryButton, Stack } from "office-ui-fabric-react";
+import { TextField } from "office-ui-fabric-react/lib/TextField";
+import { Toggle } from "office-ui-fabric-react/lib/Toggle";
 
 const stackTokens = { childrenGap: 30 };
 
@@ -13,29 +13,41 @@ export class Notifications extends React.Component {
     super(props);
     this.state = {
       postureEnabled: this.props.postureTime !== -1 ? true : false,
-      notificationsPostureTime: '',
-    }
+      notificationsPostureTime: "",
+    };
   }
 
   render() {
     return (
       <div className="notifications-container">
-        <Stack horizontal tokens={stackTokens} verticalAlign="start" className="notifications">
-          <Toggle 
+        <Stack
+          horizontal
+          tokens={stackTokens}
+          verticalAlign="start"
+          className="notifications"
+        >
+          <Toggle
             checked={this.state.postureEnabled}
             onChange={this.handlePostureToggle}
-            label="Receive posture check notifications" 
-            onText="Yes" offText="No" />
-          <TextField 
-            value={this.state.postureTime}
-            onChange={this.handlePostureChange}
-            label="Frequency of notifications in minutes" 
-            id={"posture-notification-title"} />
+            label="Receive posture check notifications"
+            onText="On"
+            offText="Off"
+          />
+          <div>
+            {this.state.postureEnabled ? (
+              <TextField
+                value={this.state.postureTime}
+                onChange={this.handlePostureChange}
+                label="Frequency of notifications in minutes"
+                id={"posture-notification-title"}
+              />
+            ) : null}
+          </div>
         </Stack>
 
         <PrimaryButton
           onClick={() => {
-            this.generateNotification()
+            this.generateNotification();
           }}
         >
           Save settings
@@ -43,26 +55,33 @@ export class Notifications extends React.Component {
       </div>
     );
   }
-  
+
   // ev is of type React.MouseEvent<HTMLElement> btw!
   handlePostureToggle = (ev, checked) => {
-    this.setState({postureEnabled: checked});
-  }
+    this.setState({ postureEnabled: checked });
+  };
 
   handlePostureChange = (event) => {
-    this.setState({notificationsPostureTime: event.target.value});
-
-  }
+    this.setState({ notificationsPostureTime: event.target.value });
+  };
 
   generateNotification() {
     var postureTime = this.state.notificationsPostureTime;
     var validTime = true;
 
     if (this.state.postureEnabled) {
-      if (!isNaN(postureTime) && Math.floor(postureTime) > 0 && postureTime !== '') {
+      if (
+        !isNaN(postureTime) &&
+        Math.floor(postureTime) > 0 &&
+        postureTime !== ""
+      ) {
         var message = `Setting posture notifications to repeat every ${postureTime} minutes. `;
         this.props.setPostureTime(postureTime);
-        this.displayNotification("Updated notification preferences", message, "default");
+        this.displayNotification(
+          "Updated notification preferences",
+          message,
+          "default"
+        );
       } else {
         this.timeSetError();
       }
@@ -70,7 +89,11 @@ export class Notifications extends React.Component {
   }
 
   timeSetError() {
-    this.displayNotification("Error!", "Please specify a valid time in minutes", "danger");
+    this.displayNotification(
+      "Error!",
+      "Please specify a valid time in minutes",
+      "danger"
+    );
   }
 
   displayNotification(title, message, type) {
