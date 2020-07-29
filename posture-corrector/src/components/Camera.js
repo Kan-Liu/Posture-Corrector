249b5d6d56e353f01c517bfea8f614e17883f087
Kan Liu <liukan07@berkeley.edu>
2020-07-29 11:15:38 -0700

    calibration implemented, notification enabled and anti-slouching is working

--- posture-corrector/src/components/Camera.js
+++ posture-corrector/src/components/Camera.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Webcam from "react-webcam";
-import '../App.css';
+import "../App.css";
 import Sketch from "react-p5";
 import * as PoseDetection from "../backend/PoseDetection";
-import { PrimaryButton, CompoundButton, Stack } from 'office-ui-fabric-react';
- 
+import { PrimaryButton, CompoundButton, Stack } from "office-ui-fabric-react";
+import * as ControlLogic from "./ControlLogic";
+
 const videoConstraints = {
-  facingMode: "user"
+  facingMode: "user",
 };
 
 export const Camera = () => {
   const webcamRef = React.useRef(null);
   const stackTokens = { childrenGap: 40 };
   const [webcamEnabled, setWebcamEnabled] = React.useState(false);
+  const [goodReference, setGoodReference] = React.useState(null);
+  const [badReference, setBadReference] = React.useState(null);
+  const [lastButton, setLastButton] = React.useState(null);
   const [lastImage, setLastImage] = React.useState(null);
 
   const capture = React.useCallback(async () => {
@@ -27,43 +31,66 @@ export const Camera = () => {
       console.log("Loaded image!");
       var keypoints = await PoseDetection.returnPose(img);
       console.log(keypoints);
+      if (lastButton === "capture") {
+        ControlLogic.anti_slouching(goodReference, badReference, keypoints);
+      } else if (lastButton === "goodReference") {
+        setGoodReference(keypoints);
+      } else if (lastButton === "badReference") {
+        setBadReference(keypoints);
+      }
     };
-  }, [webcamRef]);
+  }, [webcamRef, goodReference, badReference, lastButton]);
 
-  const onCaptureClick = () => {
+  const onCaptureClick = function (type) {
+    console.log(type);
+    setLastButton(type);
     setWebcamEnabled(true);
   };
 
-  return (<>
-    <div className="camera-container">
-      <Stack horizontal tokens={stackTokens}>
-          <CompoundButton onClick={onCaptureClick} secondaryText="The posture you wish to erase">
-              Capture your normal pose 
+  return (
+    <>
+      <div className="camera-container">
+        <Stack horizontal tokens={stackTokens}>
+          <CompoundButton
+            onClick={() => onCaptureClick("badReference")}
+            secondaryText="The posture you wish to erase"
+          >
+            Capture your normal pose
           </CompoundButton>
-          <CompoundButton primary onClick={onCaptureClick} secondaryText="Shoulders straight, arms relaxed">
-              Capture your desired pose
+          <CompoundButton
+            primary
+            onClick={() => onCaptureClick("goodReference")}
+            secondaryText="Shoulders straight, arms relaxed"
+          >
+            Capture your desired pose
           </CompoundButton>
-      </Stack>
+        </Stack>
 
-      <div style={{paddingBottom: "3vh"}}></div>
+        <div style={{ paddingBottom: "3vh" }}></div>
 
-      {webcamEnabled ? (<Webcam
-        audio={false}
-        screenshotFormat="image/jpeg"
-        width={640}
-        height={360}
-        ref={webcamRef}
-        videoConstraints={videoConstraints}
-        onUserMedia={() => {
-          setTimeout(capture, 200); }}
-      />) : (<img src={lastImage} />)}
-    </div>
+        {webcamEnabled ? (
+          <Webcam
+            audio={false}
+            screenshotFormat="image/jpeg"
+            width={640}
+            height={360}
+            ref={webcamRef}
+            videoConstraints={videoConstraints}
+            onUserMedia={() => {
+              setTimeout(capture, 2000);
+            }}
+          />
+        ) : (
+          <img src={lastImage} />
+        )}
+      </div>
 
-    <div style={{paddingBottom: "3vh"}}></div>
+      <div style={{ paddingBottom: "3vh" }}></div>
 
-    <PrimaryButton 
-      text="Capture Photo"
-      onClick={onCaptureClick}
-    />
-  </>);
+      <PrimaryButton
+        text="Capture Photo"
+        onClick={() => onCaptureClick("capture")}
+      />
+    </>
+  );
 };
--- posture-corrector/src/components/ControlLogic.js
+++ posture-corrector/src/components/ControlLogic.js
@@ -1,3 +1,5 @@
+import { store } from "react-notifications-component";
+
 const ANTI_SLOUCHING_THRESHOLD = 10;
 const TOO_CLOSE_TO_CAMERA_THRESHOLD = 10;
 const FORWARD_LEANING_THRESHOLD = 10;
@@ -7,16 +9,31 @@ const LONG_SCREEN_TIME_THRESHOLD = 10;
 // Left:x > Right:x
 // Shoulder:y > Nose:y
 
-function anti_slouching(reference, current) {
-  var reference_score =
-    (reference.rightShoulder.y + reference.leftShoulder.y) / 2 -
-    reference.nose.y;
-  var current_score =
-    (current.rightShoulder.y + current.leftShoulder.y) / 2 - current.nose.y;
-  if (reference_score - current_score > ANTI_SLOUCHING_THRESHOLD) {
-    return true;
-  } else {
-    return false;
+export function anti_slouching(good_reference, bad_reference, current) {
+  // Smaller score is worse
+  var calculate_score = (keypoints) => {
+    return (
+      (keypoints.rightShoulder.y + keypoints.leftShoulder.y) / 2 -
+      keypoints.nose.y
+    );
+  };
+  var good_reference_score = calculate_score(good_reference);
+  var bad_reference_score = calculate_score(bad_reference);
+  var current_score = calculate_score(current);
+  var good_bad_midpoint = (good_reference_score + bad_reference_score) / 2;
+
+  console.log(
+    good_reference_score,
+    bad_reference_score,
+    current_score,
+    good_bad_midpoint
+  );
+
+  if (current_score < good_bad_midpoint) {
+    trigger_notification(
+      "Anti-slouching Warning",
+      "Slouching detected. Please sit up straight!"
+    );
   }
 }
 
@@ -44,7 +61,7 @@ function forward_leaning(reference, current) {
   }
 }
 
-function tilted_head(current) {
+function tilted_head(reference, current) {
   var right_score = current.rightShoulder.y - reference.rightEar.y;
   var left_score = current.leftShoulder.y - current.leftEar.y;
   if (Math.abs(right_score - left_score) > TILTED_HEAD_THRESHOLD) {
@@ -55,3 +72,18 @@ function tilted_head(current) {
 }
 
 function long_screen_time(reference, current) {}
+
+function trigger_notification(title, reason, type) {
+  type = type || "warning";
+  store.addNotification({
+    title: title,
+    message: reason,
+    type: type, // 'default', 'success', 'info', 'warning'
+    container: "bottom-left", // where to position the notifications
+    animationIn: ["animated", "fadeIn"], // animate.css classes that's applied
+    animationOut: ["animated", "fadeOut"], // animate.css classes that's applied
+    dismiss: {
+      duration: 7500,
+    },
+  });
+}
